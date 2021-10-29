51fcbd8c3e5ea510262df2b8139030877c4dd644
Utkarsh <utkarshsingh100.us@gmail.com>
2021-10-29 17:05:27 +0530

    Initialized

---
+++ src/test/java/com/ERP/ERPAPI/ErpApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ERP.ERPAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
